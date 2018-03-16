3558b81027b0cc31b3577a34a1d19c4139476a5a
barisemreefe <efe.barisemre@gmail.com>
2018-03-16 17:53:11 -0400

    init

---
+++ app/src/main/java/com/bee/traderev/utils/TradeRevApplication.kt
@@ -0,0 +1,17 @@
+package com.bee.traderev.utils
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class TradeRevApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initializeLeakCanary()
+    }
+    private fun initializeLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
+    }
+}
